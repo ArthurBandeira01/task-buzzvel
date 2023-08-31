098d6d6e6debcb0a41b6bd3545f74874c13f1d83
Arthur Bandeira <arthurbandeirafc@gmail.com>
2023-08-31 14:28:30 -0300

    First commit

---
+++ app/Http/Controllers/api/TasksController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+   
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+   
+    public function destroy(string $id)
+    {
+        //
+    }
+}
